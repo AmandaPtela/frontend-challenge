4e910963ca06a4b0c328ae815cfc984f6bf9ce7c
Amanda Portela <mandaptela@gmail.com>
2024-03-06 16:03:58 -0300

    implementação inicial da área de tarefas

--- frontend-challenge/src/Components/Tarefas.jsx
+++ frontend-challenge/src/Components/Tarefas.jsx
@@ -1,11 +1,33 @@
+import { useContext, useState } from "react";
 import { Table, DisplayTarefas, InputAdd, TBody } from "../styles";
+import { Context } from "../Context/Context";
 
 export default function Tarefas() {
+
+    const initialItem = {
+        value: "",
+        status: "pending"
+    }
+    const { items, setItem } = useContext(Context);
+    let [newItem, setNewItem] = useState("");
+
+    const createNewItem = (create) => {
+        const listItems = items;
+        newItem = { value: create, status: initialItem.status}
+        listItems.push(newItem);
+        return setItem(listItems);
+    }
+
     return (
         <DisplayTarefas>
             <div id="newItemArea" style={{ display: "flex", alignItems: "center" }}>
-                <InputAdd type="text" placeholder="Add new item" />
-                <button style={{ display: "flex", alignItems: "center", justifyContent: "center", width: "47px", height: "50px", border: "none", backgroundColor: "#4DA6B3" }}>
+                <InputAdd type="text" onChange={({ target }) => setNewItem(target.value)} value={newItem} placeholder="Add new item" />
+                <button style={{ display: "flex", alignItems: "center", justifyContent: "center", width: "47px", height: "50px", border: "none", backgroundColor: "#4DA6B3" }}
+                    onClick={() => {
+                        createNewItem(newItem);
+                        setNewItem("")
+                    }}
+                >
                     <span
                         style={{
                             display: "flex",
@@ -18,14 +40,16 @@ export default function Tarefas() {
                             fontWeight: "500",
                             color: "#4DA6B3",
                         }}>
-                            +
+                        +
                     </span>
                 </button>
             </div>
             <Table>
-                <TBody style={{ color: "#848484" }}>
-                    <tr>tarefa1</tr>
-                </TBody>
+                {items.map((item, index) => (
+                    <TBody key={index} style={{ color: "#848484" }}>
+                        <tr><th>{item.value}</th></tr>
+                    </TBody>
+                ))}
             </Table>
         </DisplayTarefas>
     )
