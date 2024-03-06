d62e1f61ada2e7335bd78d6781bc0a86e9781548
Amanda Portela <mandaptela@gmail.com>
2024-03-06 15:19:03 -0300

    comentário

--- frontend-challenge/src/Components/ProgressBar.jsx
+++ frontend-challenge/src/Components/ProgressBar.jsx
@@ -2,5 +2,6 @@ import { Bar } from "../styles";
 export default function ProgressBar() {
     return(
         <Bar>Barra de progresso de acordo com as tarefas</Bar>
+        /* cor do progresso: #5DE290 */
     )
 }
