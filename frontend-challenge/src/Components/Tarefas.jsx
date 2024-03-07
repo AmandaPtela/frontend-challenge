3a0c7c8b87ba501e202fb18f481ea59a52ae410d
Amanda Portela <mandaptela@gmail.com>
2024-03-07 20:23:11 -0300

    funcoes para 'concluir', 'retornar' e 'deletar' tarefa adicionadas

--- frontend-challenge/src/Components/Tarefas.jsx
+++ frontend-challenge/src/Components/Tarefas.jsx
@@ -1,21 +1,97 @@
-import { useContext, useState } from "react";
-import { Table, DisplayTarefas, InputAdd, TBody } from "../styles";
+import { useContext, useState, useEffect } from "react";
+import { Table, DisplayTarefas, InputAdd, TBody, TRow, StyledImage } from "../styles";
 import { Context } from "../Context/Context";
 
 export default function Tarefas() {
 
     const initialItem = {
         value: "",
-        status: "pending"
+        status: "Pending"
     }
-    const { items, setItem } = useContext(Context);
+    const { items, setItem, filtered, setFiltered, setDoneTasks, setPercentage } = useContext(Context);
     let [newItem, setNewItem] = useState("");
+    let [done, setDone] = useState(true);
+
+    useEffect(() => setFiltered(items), [items, setFiltered]);
 
     const createNewItem = (create) => {
         const listItems = items;
-        newItem = { value: create, status: initialItem.status}
+        newItem = { value: create, status: initialItem.status }
         listItems.push(newItem);
         return setItem(listItems);
+    }
+
+    const deleteItem = (item) => {
+        const newList = items.filter((tarefas) => tarefas.value !== item);
+        return setItem(newList);
+    }
+
+    const renderItem = (item, index) => {
+        if (item.status === "Done") {
+            return (
+                <TRow key={index} id={`${index}-done`} >
+                    <tr>
+                        <th style={{ textDecoration: "line-through", zIndex: 0 }}>{item.value}</th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <button
+                                onClick={() => {
+                                    setDone(!done);
+                                    item.status = done ? "Done" : "Pending";
+                                    setDoneTasks(items.filter((tarefa) => tarefa.status !== "Pending"));
+                                    setPercentage(items.filter((tarefas) => tarefas.status === "Done").length * 10)
+                                }}
+                                style={{ border: "none", outline: "none", backgroundColor: "transparent", cursor: "pointer", zIndex: 2 }}
+                                >
+                                    <StyledImage id="return-item" alt="return-item" height="30px" />
+                                </button>
+                            <button
+                                onClick={() => {
+                                    deleteItem(item.value);
+                                }}
+                                style={{ border: "none", outline: "none", backgroundColor: "transparent", cursor: "pointer", zIndex: 2 }}
+                            >
+                                <StyledImage id="delete-item" alt="delete-item" height="30px" />
+                            </button>
+
+                        </td>
+                    </tr>
+                </TRow>
+            )
+        }
+
+        return (
+            <TRow key={index} id={`${index}-pending`} >
+                <tr>
+                    <th style={{zIndex: 0}}>{item.value}</th>
+                </tr>
+                <tr>
+                    <td>
+
+                        <button
+                            onClick={() => {
+                                setDone(!done);
+                                item.status = done ? "Done" : "Pending";
+                                setDoneTasks(items.filter((tarefas) => tarefas.status === "Done"));
+                                setPercentage(items.filter((tarefas) => tarefas.status === "Done").length * 10)
+                            }}
+                            style={{ border: "none", outline: "none", backgroundColor: "transparent", cursor: "pointer", zIndex: 2 }}
+                        >
+                            <StyledImage id="item-done" alt="item-done" height="30px" />
+                        </button>
+                        <button
+                            onClick={() => {
+                                deleteItem(item.value);
+                            }}
+                            style={{ border: "none", outline: "none", backgroundColor: "transparent", cursor: "pointer", zIndex: 2 }}
+                        >
+                            <StyledImage id="delete-item" alt="delete-item" height="30px" />
+                        </button>
+                    </td>
+                </tr>
+            </TRow>
+        )
     }
 
     return (
@@ -45,11 +121,9 @@ export default function Tarefas() {
                 </button>
             </div>
             <Table>
-                {items.map((item, index) => (
-                    <TBody key={index} style={{ color: "#848484" }}>
-                        <tr><th>{item.value}</th></tr>
-                    </TBody>
-                ))}
+                <TBody>
+                    {filtered.map((item, index) => renderItem(item, index))}
+                </TBody>
             </Table>
         </DisplayTarefas>
     )
