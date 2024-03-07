c32b9e2246d5595c78f362c20727511647794e54
Amanda Portela <mandaptela@gmail.com>
2024-03-07 20:21:31 -0300

    implementacao da funcao de filtragem

--- frontend-challenge/src/Components/Filters.jsx
+++ frontend-challenge/src/Components/Filters.jsx
@@ -4,36 +4,64 @@ import { DisplayFiltersBtn, DisplayFilters, Filter, FilterInput } from "../style
 
 export default function Filters() {
     let [active, setActive] = useState("");
-    const values = useContext(Context)
+    const values = useContext(Context);
+    const { items, setFiltered, setFilter, setDoneTasks } = values;
+
+    function filterItems(filter) {
+        if (filter === 'done') {
+            setDoneTasks(items.filter((tarefa) => tarefa.status === "Done"));
+            return setFiltered(items.filter((tarefa) => tarefa.status === "Done"));
+        }
+        if (filter === 'pend') {
+            return setFiltered(items.filter((tarefa) => tarefa.status === "Pending"));
+        }
+        return setFiltered(items);
+    }
 
     return (
         <DisplayFilters>
             <DisplayFiltersBtn>
                 <Filter
                     name="done"
-                    onClick={(e) => {
-                        setActive("done-active")
-                        values.setFilter(e.target.name)
+                    onClick={({ target }) => {
+                        setActive("done-active");
+                        setFilter(target.name);
+                        filterItems(target.name);
                     }}
                     className={active === "done-active" ? active : "done"}
                 >
                     Done
                 </Filter>
                 <Filter
-                    name="pending"
-                    onClick={(e) => {
-                        setActive("pend-active")
-                        values.setFilter(e.target.name)
+                    name="pend"
+                    onClick={({ target }) => {
+                        setActive("pend-active");
+                        setFilter(target.name);
+                        filterItems(target.name);
                     }}
                     className={active === "pend-active" ? active : "pend"}
                 >
                     Pending
                 </Filter>
+                <Filter
+                    name="all"
+                    onClick={({ target }) => {
+                        setActive("all-active");
+                        setFilter(target.name);
+                        filterItems(target.name);
+                    }}
+                    className={active === "all-active" ? active : "pend"}
+                >
+                    All
+                </Filter>
             </DisplayFiltersBtn>
             <FilterInput
                 type="text"
                 name="text-filter"
-                onChange={({target}) => values.setFilter(target.value)}
+                onChange={({ target }) => {
+                    filterItems(target.name);
+                    setFilter(target.value)
+                }}
                 placeholder="Search items"
             />
         </DisplayFilters>
