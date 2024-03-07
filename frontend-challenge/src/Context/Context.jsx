106656c5605c9fc1ee1d2a2b7d68ec34e1deb29a
Amanda Portela <mandaptela@gmail.com>
2024-03-07 20:23:51 -0300

    propriedades adicionadas para uso nos componentes 'ProgressBar', 'Filters' e 'Tarefas'

--- frontend-challenge/src/Context/Context.jsx
+++ frontend-challenge/src/Context/Context.jsx
@@ -5,10 +5,25 @@ export const Context = createContext();
 
 export const Provider = ({ children }) => {
     const [filter, setFilter] = useState("");
+    const [filtered, setFiltered] = useState([]);
     const [items, setItem] = useState([]);
+    const [doneTasks, setDoneTasks] = useState([]);
+    const [percentage, setPercentage] = useState(0);
 
     return (
-        <Context.Provider value={{ filter, setFilter, items, setItem }}>
+        <Context.Provider
+            value={{
+                filter,
+                setFilter,
+                items,
+                setItem,
+                filtered,
+                setFiltered,
+                doneTasks,
+                setDoneTasks,
+                percentage,
+                setPercentage
+            }}>
             {children}
         </Context.Provider>
     )
