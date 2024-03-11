c662cff23144b9dac8e9fe002c436d2ab86ca5c4
Amanda Portela <mandaptela@gmail.com>
2024-03-10 21:23:11 -0300

    ajuste no incremento da barra de progresso de acordo com os itens já feitos

--- frontend-challenge/src/Components/ProgressBar.jsx
+++ frontend-challenge/src/Components/ProgressBar.jsx
@@ -6,29 +6,47 @@ import css from "@styled-system/css";
 
 export default function ProgressBar() {
 
-    const { percentage } = useContext(Context);
+    const { doneTasks, items } = useContext(Context);
+    let initial = 0;
+    const done = doneTasks.length;
+    let taskPercentage = Number(items.length === 0 ? 0 : (100 / items.length).toFixed(0));
+
+    const setPercentage = () => {
+
+        for (let i = 0; i <= done; i += 1) {
+            if (i === 1) {
+                initial = taskPercentage;
+            }
+
+            if (i > 1) {
+                initial = initial + taskPercentage;
+            }
+        }
+    }
+
+    setPercentage()
 
     const increase = keyframes`
         from {
-            width: ${percentage - 10}% 
+            width: ${initial - taskPercentage}% 
         }
         to {
-            ${percentage}%
+            ${initial}%
         }
     
     `
     const style = {
         backgroundColor: "#5DE290",
-        width: `${percentage}%`,
+        width: `${initial}%`,
         maxWidth: "100%",
         height: "30px",
         margin: 0,
         border: "none",
-        transition: "1.5s",
+        transition: "1s",
         animation: css`${increase} 1.5s linear 1`
     }
     return (
-        <DisplayProgressBar>
+        <DisplayProgressBar size={initial}>
             < hr style={style} />
         </DisplayProgressBar>
     )
